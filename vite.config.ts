d03ae3eb4b22d693d25360aea555e0a45e65ade6
sakshi-gor <70909701+sakshi-gor@users.noreply.github.com>
2026-01-19 00:01:17 -0500

    Add changes for portfolio visibility to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/portfolio/',
   server: {
     host: "::",
     port: 8080,
@@ -14,5 +15,9 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: ["react", "react-dom"],
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
   },
 }));
